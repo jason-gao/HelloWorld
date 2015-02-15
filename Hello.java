2f561e250507d82ea781785ae8ad2ab14cd7339f
jasong <3048789891@qq.com>
2015-02-15 07:03:53 -0800

    add Hello java Comment

--- Hello.java
+++ Hello.java
@@ -1,20 +1,22 @@
 public class Hello{
 	
+	//属性
 	private Integer a;
 	
+	//构造函数
 	public Hello(){
 		this.a = 10;
 		System.out.println("Hello Construct...");
 	}
-	
+	//成员方法
 	public void test(){
 		System.out.println("Hello test");
 	}
-	
+	//打印成员变量
 	public void printValue(){
 		System.out.println(this.a);
 	}
-	
+	//主函数
 	public static void main(String[] args){
 		System.out.println("Hello!!");
 		Hello h = new Hello();
